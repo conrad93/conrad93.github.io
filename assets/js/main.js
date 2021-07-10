fdf9a756af4d308ca1ed3ea94eb29ac193dc7b8d
Conrad Nunes <nunes.conrad93@gmail.com>
2021-07-10 20:50:38 +0530

    date & time

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ var cursor2 = document.getElementById("cursor2");
 var cursor3 = document.getElementById("cursor3");
 var cursor4 = document.getElementById("cursor4");
 var cursor5 = document.getElementById("cursor5");
+var cursor6 = document.getElementById("cursor6");
+var dateTime = document.getElementById("date-time");
 var timeout;
 
 switchInput[0].addEventListener('change', () => {
@@ -15,12 +17,14 @@ switchInput[0].addEventListener('change', () => {
 });
 
 function loaderFunction() {
+    setTimeout(() => {
         loader[0].style.opacity = 0;        
         loader[0].style.display = "none";
         main[0].style.display = "block";
         setTimeout(() => {
             main[0].style.opacity = 1;
         }, 50);
+    }, 500);    
 }
 
 window.addEventListener('load', loaderFunction);
@@ -29,8 +33,8 @@ document.addEventListener("mousemove", (e) => {
     let x = e.pageX;
     let y = e.pageY;
 
-    cursor1.style.top = cursor2.style.top = cursor3.style.top = cursor4.style.top = cursor5.style.top = y + "px";
-    cursor1.style.left = cursor2.style.left = cursor3.style.left = cursor4.style.left = cursor5.style.left = x + "px";
+    cursor1.style.top = cursor2.style.top = cursor3.style.top = cursor4.style.top = cursor5.style.top = cursor6.style.top = y + "px";
+    cursor1.style.left = cursor2.style.left = cursor3.style.left = cursor4.style.left = cursor5.style.left = cursor6.style.left = x + "px";
     Array.from(cursor).forEach(element => {
         element.style.display = "block";
     });
@@ -41,7 +45,7 @@ document.addEventListener("mousemove", (e) => {
         });
     }
     clearTimeout(timeout);
-    timeout = setTimeout(mouseStopped, 1000);
+    timeout = setTimeout(mouseStopped, 3000);
 
 });
 
@@ -50,3 +54,7 @@ document.addEventListener("mouseout", () => {
         element.style.display = "none";
     });
 });
+
+setInterval(() => {
+    dateTime.innerHTML = new Date().toDateString() + ", " + new Date().toLocaleTimeString();
+}, 1000);
