5306d93d76f0b63ffc3f32f6ef9daae7f958875d
Conrad Nunes <nunes.conrad93@gmail.com>
2022-09-28 21:33:23 +0530

    v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,14 +16,14 @@ switchInput[0].addEventListener('change', () => {
 });
 
 function loaderFunction() {
-    setTimeout(() => {
-        loader[0].style.opacity = 0;        
-        loader[0].style.display = "none";
-        main[0].style.display = "block";
-        setTimeout(() => {
-            main[0].style.opacity = 1;
-        }, 50);
-    }, 500);    
+    // setTimeout(() => {
+    //     loader[0].style.opacity = 0;
+    //     loader[0].style.display = "none";
+    //     main[0].style.display = "block";
+    //     setTimeout(() => {
+    //         main[0].style.opacity = 1;
+    //     }, 50);
+    // }, 500);
 }
 
 window.addEventListener('load', loaderFunction);
